00a732645979101143f98aa1e9c7ae2cae91423b
Muller Esposito <mulleresposito@hotmail.com>
2024-10-17 22:43:07 -0300

    styles(Global): it creates the global style to the application;

---
+++ Front End (React)/web/src/styles/mixins.ts
@@ -0,0 +1,12 @@
+import { css } from 'styled-components'
+
+export const mixins = {
+  fonts: {
+    text: css`
+      font-family: 'Roboto', sans-serif;
+      font-size: 0.5rem;
+      line-height: 100%;
+      font-weight: 100;
+    `,
+  },
+}
