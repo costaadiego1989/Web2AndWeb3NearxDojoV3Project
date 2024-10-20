8007e4b984a58fc76a3139b23f7e6ac99181e29c
Muller Esposito <mulleresposito@hotmail.com>
2024-10-20 15:00:47 -0300

    styles(global): it adds more colors to the global object colors;

--- Front End (React)/web/src/styles/colors.ts
+++ Front End (React)/web/src/styles/colors.ts
@@ -3,6 +3,10 @@ export const colors = {
     on: "#008645",
     off: "#d32417",
     preoff: "#a19c2a",
-    preon: "#269b89"
+    preon: "#269b89",
+  },
+  background: {
+    red: "#ed6d64",
+    blue: "#b4e1f5",
   }
 }
